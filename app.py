1c590bf15c0f6d8f74a1c994816a3a7b8fa640f7
SAINT <dkemboi2003@gmail.com>
2026-07-05 23:45:49 +0300

    Add template inheritance and multiple routes

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", title = "Home")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html", title = "Resume")
+
+@app.route("/cover-letter")
+def cover_letter():
+    return render_template("cover_letter.html", title = "Cover Letter")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title = "Login")
 
 if __name__ == "__main__":
     app.run(debug=True)
